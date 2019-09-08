f3956b72d5e0d43001cadee300a6183a412b5c4b
root <root@terraform-instance.asia-south1-c.c.zabbacar-1543945857703.internal>
2019-09-08 15:26:27 +0000

    Downgraded to v1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Tab from '@material-ui/core/Tab';
 
 import Product from './Components/Product';
 import ProductListing from './Components/ProductListing';
-import ProductRecall from './Components/ProductRecall';
 
 function a11yProps(index) {
   return {
@@ -39,17 +38,11 @@ function App() {
           aria-label="scrollable auto tabs example"
         >
           <Tab icon={<img src={"https://logos-download.com/wp-content/uploads/2016/08/EY_logo_slogan.png"} height={"42"} />} {...a11yProps(0)} />
-          <Tab label="Product Recall" {...a11yProps(1)} />
         </Tabs>
       </AppBar>
 
       <header className="App-header">
-      {
-        value == 0 ?
-        <ProductListing /> :
-        <ProductRecall />
-      }
-
+      <ProductListing />
       </header>
     </div>
   );
